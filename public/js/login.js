85b847827c5542cd88daffd4281b623949e7308c
dcemmerson <emmerson@oregonstate.edu>
2019-11-26 18:18:22 -0800

    it's a start

---
+++ public/js/login.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded',() =>{
+    document.getElementById('loginButton').addEventListener('click',async () =>{
+	let context = {};
+	context.username = document.getElementById('username').value;
+	context.password = document.getElementById('password').value;
+
+	try{
+	    const response = await fetch('/login', {
+		method: 'POST',
+		mode: 'cors',
+		cache: 'no-cache',
+		credentials: 'same-origin',
+		headers: {
+		    'Content-Type': 'application/json'
+		},
+		//	    redirect: 'follow', // manual, *follow, error
+		//	    referrer: 'no-referrer', // no-referrer, *client
+		body: JSON.stringify(context) // body data type must match "Content-Type" header
+	    })
+	    
+	    response = await response.json();
+	    
+	    if(response.invalidCredentials){
+		let feedback = document.getElementById('feedbackDiv');
+		feedback.style.color = red;
+		feedback.innerText = "Invalid login. Please try again.";
+	    }
+	}
+	catch(err){
+	    console.log(err);
+	}
+	
+    });
+});
---
+++ server.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+const url = require('url');
+
+var session = require('express-session');
+var mysql = require('./dbcon.js');
+
+let hbs = require('express-handlebars').create({
+    defaultLayout: 'main',
+    extname: 'hbs',
+    layoutDir: `${__dirname}/views/layouts`,
+    partialsDir: `${__dirname}/views/partials`
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: 'password',
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: {
+        path: '/',
+	secure: false,
+        httpOnly: true,
+        maxAge: 600 * 100000
+    }
+
+}));
+app.use(express.static('public'));
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('port', 10001);
+
+let cssFile;
+app.get(`/css/${cssFile}`, function(req,res){
+    res.send(`/css/${cssFile}`);
+    res.end;
+});
+let jsFile;
+app.get(`/js/${jsFile}`, function(req,res){
+    res.send(`/js/${jsFile}`);
+    res.end();
+});
+let imgFile;
+app.get(`/img/${imgFile}`, function(req,res){
+    res.send(`/img/${imgFile}`);
+    res.end();
+});
+
+
+app.post('/login',function(req,res){
+    let context = {};
+    let sql = `SELECT username,password FROM users WHERE username=?`;
+    let values = req.body.username;
+    
+    mysql.pool(sql,values,results => {
+	if(results[0] && results[0].password === req.body.password)
+	    res.render('landingPage');
+	else{
+	    context.invalidCredentials = true;
+	    res.send(context);
+	}
+    });
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
